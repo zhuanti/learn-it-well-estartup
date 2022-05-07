a428068088ab60de3531c232d5d23c1f694e91e1
yuki <10756044@ntub.edu.tw>
2022-05-07 18:19:01 +0800

    start

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$^cj&s%f6ehv=aa44rh1l*j-_2ke%%y_#m3=)$e-09e^bggjxv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['learn-it-well-estartup.herokuapp.com']
 #learn-it-well-estartup.herokuapp.com
 
 # Application definition
