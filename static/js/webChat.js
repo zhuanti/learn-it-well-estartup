71ecbcfaf6ae6bd21c01bc28be1429ff74cbe2c8
yuki <10756044@ntub.edu.tw>
2022-05-07 18:26:52 +0800

    inpage chat

--- static/js/webChat.js
+++ static/js/webChat.js
@@ -1,114 +1,105 @@
-const webMessage = [
-  {
-    "no":1,
-    "name": "David",
-    "photo":"img/friends/David.png",
-    "type":"text",
-    "message":"Hi",
-    "dateTime":"09/23 10:00",
-    "me": false
-  },
-  {
-    "no":2,
-    "name": "Johnny",
-    "photo":"img/me/Johnny.png",
-    "type":"text",
-    "message":"你好",
-    "dateTime":"09/23 10:15",
-    "me": true
-  },
-  {
-    "no":3,
-    "name": "David",
-    "photo":"img/friends/David.png",
-    "type":"text",
-    "message":"推薦給你一張漂亮的底圖...",
-    "dateTime":"09/23 10:16",
-    "me": false
-  },
-  {
-    "no":4,
-    "name": "Johnny",
-    "photo":"img/me/Johnny.png",
-    "type":"text",
-    "message":"好ㄜ",
-    "dateTime":"09/23 10:18",
-    "me": true
-  },
-  {
-    "no":5,
-    "name": "David",
-    "photo":"img/friends/David.png",
-    "type":"photo",
-    "message":"img/photo/423893.jpg",
-    "dateTime":"09/23 10:19",
-    "me": false
-  },
-  {
-    "no":6,
-    "name": "Johnny",
-    "photo":"img/me/Johnny.png",
-    "type":"text",
-    "message":"謝謝，我將用在新的專案上...",
-    "dateTime":"09/23 10:20",
-    "me": true
-  },
-  {
-    "no":7,
-    "name": "David",
-    "photo":"img/friends/David.png",
-    "type":"emoji",
-    "message":"img/emoji/like.png",
-    "dateTime":"09/23 10:16",
-    "me": false
-  }
+const LEFT = "left";
+const RIGHT = "right";
+
+const EVENT_MESSAGE = "message"
+const EVENT_OTHER = "other"
+
+const userPhotos = [
+    "https://www.flaticon.com/svg/static/icons/svg/3408/3408584.svg",
+    "https://www.flaticon.com/svg/static/icons/svg/3408/3408537.svg",
+    "https://www.flaticon.com/svg/static/icons/svg/3408/3408540.svg",
+    "https://www.flaticon.com/svg/static/icons/svg/3408/3408545.svg",
+    "https://www.flaticon.com/svg/static/icons/svg/3408/3408551.svg",
+    "https://www.flaticon.com/svg/static/icons/svg/3408/3408556.svg",
+    "https://www.flaticon.com/svg/static/icons/svg/3408/3408564.svg",
+    "https://www.flaticon.com/svg/static/icons/svg/3408/3408571.svg",
+    "https://www.flaticon.com/svg/static/icons/svg/3408/3408578.svg",
+    "https://www.flaticon.com/svg/static/icons/svg/3408/3408720.svg"
 ]
+var PERSON_IMG = userPhotos[getRandomNum(0, userPhotos.length)];
+var PERSON_NAME = "Guest" + Math.floor(Math.random() * 1000);
 
-let message = '';
-webMessage.map(data => {
-  if(data.me){
-    if(data.type == 'text'){
-      message += `
-        <div class="message_row you-message">
-          <div class="message-content">
-            <div class="message-text">${data.message}</div>
-            <div class="message-time">${data.dateTime}</div>
-          </div>
-        </div>
-      `
-    } else {
-      message += `
-        <div class="message_row you-message">
-          <div class="message-content">
-            <img class="ejIcon" src="${data.message}" alt="">
-            <div class="message-time">${data.dateTime}</div>
-          </div>
-        </div>
-      `
-    }
-  } else {
-    if(data.type == 'text'){
-      message += `
-        <div class="message_row other-message">
-          <div class="message-content">
-            <img class="head" src="${data.photo}" alt="">
-            <div class="message-text">${data.message}</div>
-            <div class="message-time">${data.dateTime}</div>
-          </div>
-        </div>
-      `
-    } else {
-      message += `
-        <div class="message_row other-message">
-          <div class="message-content">
-            <img class="head" src="${data.photo}" alt="">
-            <img class="ejIcon" src="${data.message}" alt="">
-            <div class="message-time">${data.dateTime}</div>
-          </div>
-        </div>
-      `
-    }
-  }
-})
+var url = "ws://" + window.location.host + "/ws?id=" + PERSON_NAME;
+var ws = new WebSocket(url);
+var name = "Guest" + Math.floor(Math.random() * 1000);
+var chatroom = document.getElementsByClassName("msger-chat")
+var text = document.getElementById("msg");
+var send = document.getElementById("send")
 
-document.getElementById('chatRoom').innerHTML = message;
+send.onclick = function (e) {
+    handleMessageEvent()
+}
+
+text.onkeydown = function (e) {
+    if (e.keyCode === 13 && text.value !== "") {
+        handleMessageEvent()
+    }
+};
+
+ws.onmessage = function (e) {
+    var m = JSON.parse(e.data)
+    var msg = ""
+    switch (m.event) {
+        case EVENT_MESSAGE:
+            if (m.name == PERSON_NAME) {
+                msg = getMessage(m.name, m.photo, RIGHT, m.content);
+            } else {
+                msg = getMessage(m.name, m.photo, LEFT, m.content);
+            }
+            break;
+        case EVENT_OTHER:
+            if (m.name != PERSON_NAME) {
+                msg = getEventMessage(m.name + " " + m.content)
+            } else {
+                msg = getEventMessage("您已" + m.content)
+            }
+            break;
+    }
+    insertMsg(msg, chatroom[0]);
+};
+
+ws.onclose = function (e) {
+    console.log(e)
+}
+
+function handleMessageEvent() {
+    ws.send(JSON.stringify({
+        "event": "message",
+        "photo": PERSON_IMG,
+        "name": PERSON_NAME,
+        "content": text.value,
+    }));
+    text.value = "";
+}
+
+function getEventMessage(msg) {
+    var msg = `<div class="msg-left">${msg}</div>`
+    return msg
+}
+
+function getMessage(name, img, side, text) {
+    const d = new Date()
+    //   Simple solution for small apps
+    var msg = `
+    <div class="msg ${side}-msg">
+      <div class="msg-img" style="background-image: url(${img})"></div>
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+          <div class="msg-info-time">${d.getFullYear()}/${d.getMonth()}/${d.getDay()} ${d.getHours()}:${d.getMinutes()}</div>
+        </div>
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `
+    return msg;
+}
+
+function insertMsg(msg, domObj) {
+    domObj.insertAdjacentHTML("beforeend", msg);
+    domObj.scrollTop += 500;
+}
+
+function getRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
