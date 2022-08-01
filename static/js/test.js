d814573097e60ab8c9bd32532bd5cc030e7df7fe
wenyu-chen <10756027@ntub.edu.tw>
2022-08-01 11:27:52 +0800

    修改-個人自習室倒數計時時間

--- static/js/test.js
+++ static/js/test.js
@@ -1,123 +1,301 @@
-//計時器
-$(document).ready(function(){
-  // Values
-  var isPaused = true;
-  var isSession = true;
-  var breakTime = 300;
-  var breakTimer = breakTime;
-  var sessionTime = 1500;
-  var sessionTimer = sessionTime;
+//讀書時間(pomodore)為 25 分鐘，短暫的休息時間(shortBreak)為 5 分鐘。連續四次番茄工作後，將啟動長時間的休息(longBreak)（15 分鐘）
+const timer = {
+  pomodoro: 0.1,
+//  overtime: 0,
+//  shortBreak: 5,
+//  longBreak: 15,
+//  longBreakInterval: 4,
+};
 
-  // The timer
-  var t = window.setInterval(function() {
-    if(!isPaused) {
-      if (isSession) {
-        sessionTimer -= 1;
-        setTimer(sessionTimer);
-        if (sessionTimer == 0) {
-          toggleMode();
-          sessionTimer = sessionTime;
-        }
-      } else if (!isSession) {
-        breakTimer -= 1;
-        setTimer(breakTimer)
-        if (breakTimer == 0) {
-          toggleMode();
-          breakTimer = breakTime;
-        }
+//聲明一個interval變量timer
+let interval;
+
+const buttonSound = new Audio('/static/music/button-sound.mp3');
+const mainButton = document.getElementById('js-btn');
+mainButton.addEventListener('click', () => {
+  buttonSound.play();
+  const { action } = mainButton.dataset;
+  if (action === 'start') {
+    startTimer();
+  } else {
+    stopTimer();
+  }
+});
+
+const modeButtons = document.querySelector('#js-mode-buttons');
+modeButtons.addEventListener('click', handleMode);
+
+function getRemainingTime(endTime) {
+  const currentTime = Date.parse(new Date());
+  const difference = endTime - currentTime;
+
+  const total = Number.parseInt(difference / 1000, 10);
+  const minutes = Number.parseInt((total / 60) % 60, 10);
+  const seconds = Number.parseInt(total % 60, 10);
+
+  return {
+    total,
+    minutes,
+    seconds,
+  };
+}
+
+function startTimer() {
+  let { total } = timer.remainingTime;
+  const endTime = Date.parse(new Date()) + total * 1000;
+
+  if (timer.mode === 'pomodoro') timer.sessions++;
+
+  mainButton.dataset.action = 'stop';
+  mainButton.textContent = '暫停';
+  mainButton.classList.add('active');
+
+  interval = setInterval(function() {
+    timer.remainingTime = getRemainingTime(endTime);
+    updateClock();
+
+    total = timer.remainingTime.total;
+    if (total <= 0) {
+      clearInterval(interval);
+
+      switch (timer.mode) {
+        case 'pomodoro':
+          if (timer.sessions % timer.longBreakInterval === 0) {
+            switchMode('shortBreak');
+          } else {
+            switchMode('overtime');
+            document.getElementById('js-overbtn').disabled = false;
+          }
+          break;
+        default:
+          switchMode('pomodoro');
       }
+      if (Notification.permission === 'granted') {
+        const text =
+          timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+        new Notification(text);
+      }
+//          if (timer.sessions % timer.longBreakInterval === 0) {
+//            switchMode('longBreak');
+//          } else {
+//            switchMode('shortBreak');
+//          }
+//          break;
+//        default:
+//          switchMode('pomodoro');
+//      }
+//
+      if (Notification.permission === 'granted') {
+        const text =
+          timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+        new Notification(text);
+      }
+
+      document.querySelector(`[data-sound="${timer.mode}"]`).play();
+
+      startTimer();
     }
   }, 1000);
+}
 
-  // Refresh v
-  function refreshValues() {
-    $(".break-time").text(breakTime / 60);
-    $(".session-time").text(sessionTime / 60);
-    // Set clock time
-    setTimer(sessionTimer);
-  }
+function stopTimer() {
+  clearInterval(interval);
 
-  function increaseBreak() {
-    if (breakTime !== 3600) {
-      breakTime += 60;
-      breakTimer = breakTime;
-      setTimer(breakTimer);
-      refreshValues();
+  mainButton.dataset.action = 'start';
+  mainButton.textContent = '開始';
+  mainButton.classList.remove('active');
+}
+
+function updateClock() {
+  const { remainingTime } = timer;
+  const minutes = `${remainingTime.minutes}`.padStart(2, '0');
+  const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+
+  const min = document.getElementById('js-minutes');
+  const sec = document.getElementById('js-seconds');
+  min.textContent = minutes;
+  sec.textContent = seconds;
+
+  const text =
+    timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+  document.title = `${minutes}:${seconds} — ${text}`;
+
+  const progress = document.getElementById('js-progress');
+  progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
+}
+
+function switchMode(mode) {
+  timer.mode = mode;
+  timer.remainingTime = {
+    total: timer[mode] * 60,
+    minutes: timer[mode],
+    seconds: 0,
+  };
+
+  document
+    .querySelectorAll('button[data-mode]')
+    .forEach(e => e.classList.remove('active'));
+  document.querySelector(`[data-mode="${mode}"]`).classList.add('active');
+  document.body.style.backgroundColor = `var(--${mode})`;
+  document
+    .getElementById('js-progress')
+    .setAttribute('max', timer.remainingTime.total);
+
+  updateClock();
+}
+
+function handleMode(event) {
+  const { mode } = event.target.dataset;
+
+  if (!mode) return;
+
+  switchMode(mode);
+  stopTimer();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  if ('Notification' in window) {
+    if (
+      Notification.permission !== 'granted' &&
+      Notification.permission !== 'denied'
+    ) {
+      Notification.requestPermission().then(function(permission) {
+        if (permission === 'granted') {
+          new Notification(
+            'Awesome! You will be notified at the start of each session'
+          );
+        }
+      });
     }
   }
 
-  function decreaseBreak() {
-    if (breakTime !== 60) {
-      breakTime -= 60;
-      breakTimer = breakTime;
-      setTimer(breakTimer);
-      refreshValues();
-    }
-  }
-
-  function increaseSession() {
-    if (sessionTime !== 3600) {
-      sessionTime += 60;
-      sessionTimer = sessionTime;
-      setTimer(sessionTimer);
-      refreshValues();
-    }
-  }
-
-  function decreaseSession() {
-    if (sessionTime !== 60) {
-      sessionTime -= 60;
-      sessionTimer = sessionTime;
-      setTimer(sessionTimer);
-      refreshValues();
-    }
-  }
-
-  function toggleButton() {
-    if (!isPaused) {
-      $(".pause").text("開始計時");
-      isPaused = true;
-    } else {
-      $(".pause").text("暫停計時");
-      isPaused = false;
-    }
-  }
-
-  function toggleMode() {
-    isSession = !isSession;
-    if (isSession) {
-      $("body").css("background-color", "#e55");
-      $(".mode").text("讀書倒數計時:");
-      $(".pause").css("color", "#e55");
-    } else {
-      $("body").css("background-color", "#5b5");
-      $(".mode").text("休息倒數計時:");
-      $(".pause").css("color", "rgb(246, 252, 245)");
-    }
-  }
+  switchMode('pomodoro');
+});
 
 
 
-  function setTimer(seconds) {
-    var mins = Math.floor(seconds / 60);
-    var secs = seconds % 60;
-    if (Math.max(Math.floor(Math.log10(Math.abs(secs)), 0) + 1) == 1) {
-      $(".clock").text(mins + ":" + "0" + secs);
-    } else if (secs == 0) {
-      $(".clock").text(mins + ":" + "00");
-    } else {
-      $(".clock").text(mins + ":" + secs);
-    }
-  }
 
-  $(".break-up").click(function() { increaseBreak(); })
-  $(".break-down").click(function() { decreaseBreak(); })
-  $(".session-up").click(function() { increaseSession(); })
-  $(".session-down").click(function() { decreaseSession(); })
-  $(".pause").click(function() { toggleButton(); })
 
-  refreshValues();
-})
+////計時器
+//$(document).ready(function(){
+//  // Values
+//  var isPaused = true;
+//  var isSession = true;
+//  var breakTime = 300;
+//  var breakTimer = breakTime;
+//  var sessionTime = 1500;
+//  var sessionTimer = sessionTime;
+//
+//  // The timer
+//  var t = window.setInterval(function() {
+//    if(!isPaused) {
+//      if (isSession) {
+//        sessionTimer -= 1;
+//        setTimer(sessionTimer);
+//        if (sessionTimer == 0) {
+//          toggleMode();
+//          sessionTimer = sessionTime;
+//        }
+//      } else if (!isSession) {
+//        breakTimer -= 1;
+//        setTimer(breakTimer)
+//        if (breakTimer == 0) {
+//          toggleMode();
+//          breakTimer = breakTime;
+//        }
+//      }
+//    }
+//  }, 1000);
+//
+//  // Refresh v
+//  function refreshValues() {
+//    $(".break-time").text(breakTime / 60);
+//    $(".session-time").text(sessionTime / 60);
+//    // Set clock time
+//    setTimer(sessionTimer);
+//  }
+//
+//  function increaseBreak() {
+//    if (breakTime !== 3600) {
+//      breakTime += 60;
+//      breakTimer = breakTime;
+//      setTimer(breakTimer);
+//      refreshValues();
+//    }
+//  }
+//
+//  function decreaseBreak() {
+//    if (breakTime !== 60) {
+//      breakTime -= 60;
+//      breakTimer = breakTime;
+//      setTimer(breakTimer);
+//      refreshValues();
+//    }
+//  }
+//
+//  function increaseSession() {
+//    if (sessionTime !== 3600) {
+//      sessionTime += 60;
+//      sessionTimer = sessionTime;
+//      setTimer(sessionTimer);
+//      refreshValues();
+//    }
+//  }
+//
+//  function decreaseSession() {
+//    if (sessionTime !== 60) {
+//      sessionTime -= 60;
+//      sessionTimer = sessionTime;
+//      setTimer(sessionTimer);
+//      refreshValues();
+//    }
+//  }
+//
+//  function toggleButton() {
+//    if (!isPaused) {
+//      $(".pause").text("開始計時");
+//      isPaused = true;
+//    } else {
+//      $(".pause").text("暫停計時");
+//      isPaused = false;
+//    }
+//  }
+//
+//  function toggleMode() {
+//    isSession = !isSession;
+//    if (isSession) {
+//      $("body").css("background-color", "#e55");
+//      $(".mode").text("讀書倒數計時:");
+//      $(".pause").css("color", "#e55");
+//    } else {
+//      $("body").css("background-color", "#5b5");
+//      $(".mode").text("休息倒數計時:");
+//      $(".pause").css("color", "rgb(246, 252, 245)");
+//    }
+//  }
+//
+//
+//
+//  function setTimer(seconds) {
+//    var mins = Math.floor(seconds / 60);
+//    var secs = seconds % 60;
+//    if (Math.max(Math.floor(Math.log10(Math.abs(secs)), 0) + 1) == 1) {
+//      $(".clock").text(mins + ":" + "0" + secs);
+//    } else if (secs == 0) {
+//      $(".clock").text(mins + ":" + "00");
+//    } else {
+//      $(".clock").text(mins + ":" + secs);
+//    }
+//  }
+//
+//  $(".break-up").click(function() { increaseBreak(); })
+//  $(".break-down").click(function() { decreaseBreak(); })
+//  $(".session-up").click(function() { increaseSession(); })
+//  $(".session-down").click(function() { decreaseSession(); })
+//  $(".pause").click(function() { toggleButton(); })
+//
+//  refreshValues();
+//})
 
 ////時鐘
 "use strict";
