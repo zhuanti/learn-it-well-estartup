c860852474fbcc2b9fe0fa6302041eeff170a67e
yuki <10756044@ntub.edu.tw>
2022-08-23 00:28:09 +0800

    修改-提問版前後端連接

--- core/urls.py
+++ core/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
     # path('discusroom/addroom_subject/', discusroom_review_views.addroom_subject),
     # path('inpage/', discusroom_review_views.inpage),
     path('inpage/<int:pk>/', discusroom_review_views.inpage),
-    path('inpage/#letmeopen', discusroom_review_views.qus),
+    # path('inpage/#letmeopen', discusroom_review_views.qus),
     path('dis_test/', discusroom_review_views.WebChatTest),
 
     # plan
