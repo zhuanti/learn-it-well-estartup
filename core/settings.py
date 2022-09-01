84bd2d8a327c09488876faa59b17e9ffd0ecaf6a
0MoShang0 <10756032@ntub.edu.tw>
2022-09-01 14:18:43 +0800

    heroku deployment

--- core/settings.py
+++ core/settings.py
@@ -25,12 +25,12 @@ SECRET_KEY = 'django-insecure-$^cj&s%f6ehv=aa44rh1l*j-_2ke%%y_#m3=)$e-09e^bggjxv
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # 在本地連線時用
-# DEBUG = True
-# ALLOWED_HOSTS = []
+DEBUG = True
+ALLOWED_HOSTS = []
 
 # 在Heroku連線時用
-DEBUG = False
-ALLOWED_HOSTS = ['learn-it-well-estartup.herokuapp.com']
+# DEBUG = False
+# ALLOWED_HOSTS = ['learn-it-well-estartup.herokuapp.com']
 
 # Application definition
 
@@ -133,6 +133,6 @@ STATICFILES_DIRS = (
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# API_URL = 'http://localhost:9001/api/'
+API_URL = 'http://localhost:9001/api/'
 
-API_URL = 'https://learn-it-well-estartup-api.herokuapp.com/api/'
+# API_URL = 'https://learn-it-well-estartup-api.herokuapp.com/api/'
