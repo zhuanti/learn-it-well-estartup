84b0ed474eddda9bec4603fc87733de7c0a952ab
JENNY0222 <10756023@ntub.edu.tw>
2022-09-16 20:29:55 +0800

    make it better

--- core/settings.py
+++ core/settings.py
@@ -27,12 +27,12 @@ SECRET_KEY = 'django-insecure-$^cj&s%f6ehv=aa44rh1l*j-_2ke%%y_#m3=)$e-09e^bggjxv
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # 在本地連線時用
-DEBUG = True
-ALLOWED_HOSTS = []
+# DEBUG = True
+# ALLOWED_HOSTS = []
 
 # 在Heroku連線時用
-# DEBUG = False
-# ALLOWED_HOSTS = ['learn-it-well-estartup.herokuapp.com']
+DEBUG = False
+ALLOWED_HOSTS = ['learn-it-well-estartup.herokuapp.com']
 
 # Application definition
 
