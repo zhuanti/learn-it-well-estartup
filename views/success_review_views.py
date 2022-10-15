b7b638825d5ec5fb24fc2065d3822bb244654735
yuki <10756044@ntub.edu.tw>
2022-10-15 23:59:08 +0800

    修改-成就頁面

--- views/success_review_views.py
+++ views/success_review_views.py
@@ -3,7 +3,15 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from core.settings import API_URL as root
 from utils.decorators import user_login_required
+root += 'success'
 
 @user_login_required
 def achievement(request):
-    return render(request, 'achievement.html')  # 完成
+    if request.method == 'GET':
+        r = requests.get(
+            f'{root}/list/',
+            cookies={'sessionid': request.COOKIES['sessionid']}
+        )
+        result = r.json()
+        success_lists = result['data']
+        return render(request, 'achievement.html', {'success_lists': success_lists})
