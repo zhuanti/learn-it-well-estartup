a98e676c084be99ffb6626bc6e703aae482e0a36
wenyu-chen <10756027@ntub.edu.tw>
2022-10-17 18:02:51 +0800

    修改-報表使用者資訊

--- views/report_review_views.py
+++ views/report_review_views.py
@@ -8,7 +8,17 @@ root += 'report'
 
 @user_login_required
 def reportweek(request):
-    return render(request, 'ReportWeek.html')
+    user_id = request.COOKIES['user_id'],
+    # print(user_id)
+    r = requests.get(
+        f'{root}/reportweek/',
+        params={'user_id': user_id},
+        # 'user_id': request.COOKIES['user_id'],
+        cookies={'sessionid': request.COOKIES['sessionid']}
+    )
+    result = r.json()
+    user = result['data']
+    return render(request, 'ReportWeek.html', {'user': user})
 
 
 @user_login_required
