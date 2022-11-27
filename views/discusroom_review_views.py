3f2a77181c00e79277c5b0e45bfbf86d6cee7379
0MoShang0 <10756032@ntub.edu.tw>
2022-11-27 16:58:04 +0800

    修改-討論室列表內容呈現

--- views/discusroom_review_views.py
+++ views/discusroom_review_views.py
@@ -97,17 +97,17 @@ def addqus(request, pk):
 def droom(request):
     if request.method == 'POST':
         # no = request.POST['ano']
-        subject_no_id = request.POST['asubject_no']
-        schoolsys_no_id = request.POST['aschoolsys_no_id']
+        subject_no_id = request.POST.get('op_subject_no_id')
+        schoolsys_no_id = request.POST.get('op_schoolsys_no_id')
         name = request.POST['aname']
         total_people = request.POST['atotal_people']
 
         data = {
             # 'no': no,
             'subject_no_id': subject_no_id,
+            'schoolsys_no_id': schoolsys_no_id,
             'name': name,
             'total_people': total_people,
-            'schoolsys_no_id': schoolsys_no_id,
         }
 
         r = requests.post(
