c32e589fb2197a0dc8f92f894dcbd4f32d8ab0d7
Tynt-170 <10756022@ntub.edu.tw>
2022-12-13 22:02:10 +0800

    刪除-使用者編輯部分更改密碼

--- views/user_review_views.py
+++ views/user_review_views.py
@@ -34,39 +34,32 @@ def EditUserDetail(request):
         return render(request, 'editUserDetail.html', {'user': user})
 
     if request.method == 'POST':
-        if request.POST['pwd'] == request.POST['pwd2']:
-            pwd = request.POST['pwd']
-            gender = request.POST['gender']
-            name = request.POST['name']
-            live = request.POST['live']
-            borth = request.POST['borth']
-            data = {
-                'user_id': request.COOKIES['user_id'],
-                'pwd': pwd,
-                'gender': gender,
-                'name': name,
-                'live': live,
-                'borth': borth
-            }
-            r = requests.post(
-                f'{root}user/detail/edit/',
-                # params={'user_id': user_id},
-                data=data,
-                cookies={'sessionid': request.COOKIES['sessionid']}
-            )
-            result = r.json()
+        gender = request.POST['gender']
+        name = request.POST['name']
+        live = request.POST['live']
+        borth = request.POST['borth']
+        data = {
+            'user_id': request.COOKIES['user_id'],
+            'gender': gender,
+            'name': name,
+            'live': live,
+            'borth': borth
+        }
+        r = requests.post(
+            f'{root}user/detail/edit/',
+            # params={'user_id': user_id},
+            data=data,
+            cookies={'sessionid': request.COOKIES['sessionid']}
+        )
+        result = r.json()
 
-            if result['success'] is True:
-                ret = redirect('/userdetail')
-                messages.success(request, '已修改資料成功')
-                return ret
-            else:
-                messages.error(request, '生日格式填寫錯誤，請重新修改')
-                return redirect('/edituser-detail')
-        else:
-            messages.error(request, '兩次所輸入密碼不同，請重新輸入')
-            return redirect('/edituser-detail')
+        if result['success'] is True:
+            ret = redirect('/userdetail')
+            messages.success(request, '已修改資料成功')
             return ret
+        else:
+            messages.error(request, '生日格式填寫錯誤，請重新修改')
+            return redirect('/edituser-detail')
 
 
 # 修改使用者密碼
